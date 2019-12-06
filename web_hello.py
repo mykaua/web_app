3e79d278f2a2afcaba3d9510856e6cefb15f3ead
Mykola Raryk <admin@myka.pp.ua>
2019-12-06 10:56:19 +0200

    simple web_app

---
+++ web_hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello Fucking world"
+
+@app.route("/info")
+def info():
+    return "Made by Slevin Calebra"
+
+if __name__ == "__main__":
+   app.run()
